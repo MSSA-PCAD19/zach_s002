6d951bbbdbc50885c3cb0b470c59bdea44fe16b8
zschro998 <zachary.schroeder6@outlook.com>
2025-09-09 11:07:03 -0700

    Move local functions into Game class, make the member static

---
+++ Week2/week2/TicTacToe/Game.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    // class access modifier public/internal
+    internal class Game
+    {
+        // body of class Game
+        // field, property, METHOD, event - collectively known as class member
+
+        internal static char?[,] GetBoard() => new char?[3, 3];
+        // static method has no dependency on any object state
+
+        internal static void DisplayBoard(char?[,] board)
+        {
+            var hr = new string('-', 20);
+            Console.WriteLine(hr);
+
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                Console.Write('|');
+                for (int col = 0; col < board.GetLength(1); col++)
+                {
+                    char? piece;
+                    if (board[row, col] is not null)
+                        piece = board[row, col];
+                    else
+                        piece = '_';
+
+                    Console.Write($"  {piece}  |");
+
+                }
+                Console.WriteLine($"\n{hr}");
+            }
+        }
+
+        internal static bool PlaceNextHand(char?[,] board, char nextHand)
+        {
+            bool piecePlaced = false;
+            do
+            {
+                Console.WriteLine($"{nextHand} player, please place your next piece with row col e.g 0 1 for row 0 col 1");
+                string input = Console.ReadLine();
+                int row = int.Parse(input.Split(' ')[0]);
+                int col = int.Parse(input.Split(' ')[1]);
+                if (board[row, col] is null)
+                {
+                    board[row, col] = nextHand;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Position {row} {col} is already occupied by {board[row, col]}");
+                    continue;
+                    Console.WriteLine("You should not see this, because continue skips ahead to the next iteration");
+                }
+            } while (true);
+            return BoardHasSpace(board) && DetermineOutcome(board) == "Continue";
+        }
+
+        internal static bool BoardHasSpace(char?[,] board)
+        {
+            //for (int row = 0; row < board.GetLength(0); row++)
+            //{
+            //    for (int col = 0; col < board.GetLength(1); col++)
+            //    {
+            //       if (board[row, col] == null)
+            //        {
+            //            return true;
+            //        }
+
+
+            //    }
+            //}
+
+            //alt
+
+            foreach (char? item in board) // this will loop across ALL items in all dimensions
+            {
+                if (!item.HasValue)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        internal static string DetermineOutcome(char?[,] board)
+        {
+            //check rows
+            //check cols
+            //check diag
+            // - "O wins"
+            // - "X wins"
+            // - "Draw" when board is full and no win
+            // - "Continue" when board is NOT full and no win
+            string[] lines = new string[8];
+
+            lines[0] = $"{board[0, 0]}{board[0, 1]}{board[0, 2]}"; //row 0
+            lines[1] = $"{board[1, 0]}{board[1, 1]}{board[1, 2]}"; //row 1
+            lines[2] = $"{board[2, 0]}{board[2, 1]}{board[2, 2]}"; //row 2
+
+            //lines[3] = $"{board[0,0]}{board[1,0]}{board[2,0]}"; //col 0
+            //lines[4] = $"{board[0,1]}{board[1,1]}{board[2,1]}"; //col 1
+            //lines[5] = $"{board[0,2]}{board[1,2]}{board[2,2]}"; //col 2
+            for (int i = 0; i < board.GetLength(1); i++)
+            {
+                lines[i + 3] = $"{board[0, i]}{board[1, i]}{board[2, i]}";
+            }
+
+            lines[6] = $"{board[0, 0]}{board[1, 1]}{board[2, 2]}"; //diag \
+            lines[7] = $"{board[0, 2]}{board[1, 1]}{board[2, 0]}"; //diag /
+            foreach (var item in lines)
+            {
+                if (item == "OOO")
+                    return "O wins";
+
+                if (item == "XXX")
+                    return "X wins";
+            }
+            // no wins
+            if (BoardHasSpace(board))
+                return "Continue";
+            else
+                return "Draw";
+        }
+    }
+}
--- Week2/week2/TicTacToe/Program.cs
+++ Week2/week2/TicTacToe/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using TicTacToe;
+
 // create an array 2-dim 3x3 char
 // each slot should be initialized to nullable char represented by char?
 // this means the absence of data is meaningful
@@ -6,13 +8,13 @@
 // note in C# nulls are equal to each other
 
 
-char?[,] board = GetBoard();
+char?[,] board = Game.GetBoard();
 // int? x = null; // cannot initialize regular int to null, because null means referencing nothing
 // this would be the same: Nullable<int>;
 
 // display the board
 
-DisplayBoard(board);
+Game.DisplayBoard(board);
 
 // ===================
 // |  X  |  O  |     |
@@ -25,19 +27,19 @@ DisplayBoard(board);
 
 // O starts first, collect position using row,col O = 1,0 and X = 0,0
 char nextHand = 'O';
-while (PlaceNextHand(board,nextHand)) // PlaceNextHand Prompt user for the position after collecting potition
+while (Game.PlaceNextHand(board,nextHand)) // PlaceNextHand Prompt user for the position after collecting potition
     // reprint the board
 {
     // flip nextHand from O => X and X => O
-    DisplayBoard(board);
+    Game.DisplayBoard(board);
     nextHand = nextHand == 'O' ? 'X' : 'O';
 }
 
-Console.WriteLine($"Game ends in {DetermineOutcome(board)}");
-DisplayBoard(board);
+Console.WriteLine($"Game ends in {Game.DetermineOutcome(board)}");
+Game.DisplayBoard(board);
 
 
-string outcome = DetermineOutcome(board); //O wins, X wins, or draw
+string outcome = Game.DetermineOutcome(board); //O wins, X wins, or draw
 // display outcome
 // First Attempt, we will write all methods as local functions
 // challenges
@@ -46,81 +48,8 @@ string outcome = DetermineOutcome(board); //O wins, X wins, or draw
 // 3- can not automate interactions
 // 4- code is not reusable
 
-char?[,] GetBoard() => new char?[3, 3];
-
-void DisplayBoard(char?[,] board)
-{
-    var hr = new string('-', 20);
-    Console.WriteLine(hr);
-
-    for (int row = 0; row < board.GetLength(0); row++)
-    {
-        Console.Write('|');
-        for (int col = 0; col < board.GetLength(1); col++)
-        {
-            char? piece;
-            if (board[row, col] is not null)
-                piece = board[row, col];
-            else
-                piece = '_';
-
-            Console.Write($"  {piece}  |");
-
-        }
-        Console.WriteLine($"\n{hr}");
-    }
-}
-
-bool PlaceNextHand(char?[,] board, char nextHand)
-{
-    bool piecePlaced = false;
-    do
-    {
-        Console.WriteLine($"{nextHand} player, please place your next piece with row col e.g 0 1 for row 0 col 1");
-        string input = Console.ReadLine();
-        int row = int.Parse(input.Split(' ')[0]);
-        int col = int.Parse(input.Split(' ')[1]);
-        if (board[row, col] is null)
-        {
-            board[row, col] = nextHand;
-            break;
-        }
-        else
-        {
-            Console.WriteLine($"Position {row} {col} is already occupied by {board[row, col]}");
-            continue;
-            Console.WriteLine("You should not see this, because continue skips ahead to the next iteration");
-        }
-    } while (true);
-    return BoardHasSpace(board) && DetermineOutcome(board) == "Continue";
-}
-
-bool BoardHasSpace(char?[,] board)
-{
-    //for (int row = 0; row < board.GetLength(0); row++)
-    //{
-    //    for (int col = 0; col < board.GetLength(1); col++)
-    //    {
-    //       if (board[row, col] == null)
-    //        {
-    //            return true;
-    //        }
 
 
-    //    }
-    //}
-
-    //alt
-
-    foreach (char? item in board) // this will loop across ALL items in all dimensions
-    {
-        if (!item.HasValue)
-        {
-            return true;
-        }
-    }
-    return false;
-}
 
 // mine
 string DetermineOutcomeMine(char?[,] board)
@@ -160,7 +89,7 @@ string DetermineOutcomeMine(char?[,] board)
                     // check for game over
                     else
                     {
-                        if (BoardHasSpace(board))
+                        if (Game.BoardHasSpace(board))
                         {
                             response = "Continue";
                             winner = false;
@@ -182,43 +111,3 @@ string DetermineOutcomeMine(char?[,] board)
 }
 
 
-// Victor's
-string DetermineOutcome(char?[,] board)
-{
-    //check rows
-    //check cols
-    //check diag
-    // - "O wins"
-    // - "X wins"
-    // - "Draw" when board is full and no win
-    // - "Continue" when board is NOT full and no win
-    string[] lines = new string[8];
-
-    lines[0] = $"{board[0, 0]}{board[0, 1]}{board[0, 2]}"; //row 0
-    lines[1] = $"{board[1, 0]}{board[1, 1]}{board[1, 2]}"; //row 1
-    lines[2] = $"{board[2, 0]}{board[2, 1]}{board[2, 2]}"; //row 2
-
-    //lines[3] = $"{board[0,0]}{board[1,0]}{board[2,0]}"; //col 0
-    //lines[4] = $"{board[0,1]}{board[1,1]}{board[2,1]}"; //col 1
-    //lines[5] = $"{board[0,2]}{board[1,2]}{board[2,2]}"; //col 2
-    for (int i = 0; i < board.GetLength(1); i++)
-    {
-        lines[i + 3] = $"{board[0, i]}{board[1, i]}{board[2, i]}";
-    }
-
-    lines[6] = $"{board[0, 0]}{board[1, 1]}{board[2, 2]}"; //diag \
-    lines[7] = $"{board[0, 2]}{board[1, 1]}{board[2, 0]}"; //diag /
-    foreach (var item in lines)
-    {
-        if (item == "OOO")
-            return "O wins";
-
-        if (item == "XXX")
-            return "X wins";
-    }
-    // no wins
-    if (BoardHasSpace(board))
-        return "Continue";
-    else
-        return "Draw";
-}
